5dc43c15fa760acdbc9cc4798c64670c626d4c92
Alexandre DELORME <alexandre.delorme@viacesi.fr>
2024-04-04 21:29:13 +0200

    patch route implemented

--- src/views/accountViews.js
+++ src/views/accountViews.js
@@ -17,7 +17,32 @@ module.exports = {
         return User.findByPk(id);
     },
 
-    patchAccountById: async(req, res) => { return "patchAccountById" },
+    patchAccountById: async(req, res) => {
+        const id = req.params.id;
+        const { userId, roleLabel } = req.query;
+
+        const keysAvailable = ['firstname', 'lastname', 'suspend', 'address', 'imageUrl'];
+        const fieldsToUpdate = req.body;
+
+        // admin? Your account?  
+        if (roleLabel != "admin" && userId != id) return res.status(403).send("access forbidden");
+
+        const user = await User.findByPk(id);
+
+        if (!user) return res.status(404).send("user not found");
+
+        for (const key in fieldsToUpdate) {
+            if (Object.hasOwnProperty.call(fieldsToUpdate, key) && keysAvailable.find((keyAvailable) => keyAvailable == key)) {
+                if (key === 'suspend' && roleLabel != admin) return res.status(403).send("you can't update the suspend field");
+
+                user[key] = fieldsToUpdate[key];
+            }
+        }
+
+        user.save();
+
+        return user;
+    },
 
     suspendAccountById: async(req, res) => { return "suspendAccountById" }
 }
