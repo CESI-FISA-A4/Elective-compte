a7cd5f3360953d7e6e1a1e52e0198995789d5639
Alexandre DELORME <alexandre.delorme@viacesi.fr>
2024-04-04 09:31:24 +0200

    upgrade routes

--- src/views/accountViews.js
+++ src/views/accountViews.js
@@ -6,7 +6,18 @@ module.exports = {
     },
 
     getAccountById: async(req, res) => {
-        const accountId = req.params.id;
-        return User.findByPk(accountId);
+        const id = req.params.id;
+        const { userId, roleLabel } = req.query;
+
+        // admin? Your account?  
+        if (roleLabel != "admin" && userId != id) {
+            return res.status(403).send("access forbidden");
+        }
+
+        return User.findByPk(id);
     },
+
+    patchAccountById: async(req, res) => { return "patchAccountById" },
+
+    suspendAccountById: async(req, res) => { return "suspendAccountById" }
 }
